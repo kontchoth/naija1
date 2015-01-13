74880fc9df059b566e9a7ce7f57b7ac966d833d2
Hermann Kontcho <Hermann.Kontcho@jdsu.com>
2015-01-13 08:11:08 -0500

    fixed database string

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://kontchoth:Marlene1>@ds031641.mongolab.com:31641/naija',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://kontchoth:Marlene1@ds031641.mongolab.com:31641/naija',
 	//db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/naija',
 	assets: {
 		lib: {
